ff64e9b2f500a83e080045009f819f721ccc64cb
KibiAi <vetal2004v@gmail.com>
2023-10-30 18:29:01 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using lab6._3;
+
+class Program
+{
+    static void Main()
+    {
+        Quaternion q1 = new Quaternion(1, 2, 3, 4);
+        Quaternion q2 = new Quaternion(5, 6, 7, 8);
+
+        
+        Quaternion sum = q1 + q2;
+        Console.WriteLine("Результат додавання: W={0}, X={1}, Y={2}, Z={3}", sum.W, sum.X, sum.Y, sum.Z);
+
+        
+        Quaternion difference = q1 - q2;
+        Console.WriteLine("Результат віднімання: W={0}, X={1}, Y={2}, Z={3}", difference.W, difference.X, difference.Y, difference.Z);
+
+       
+        Quaternion product = q1 * q2;
+        Console.WriteLine("Результат множення: W={0}, X={1}, Y={2}, Z={3}", product.W, product.X, product.Y, product.Z);
+
+        
+        double magnitude = q1.Magnitude();
+        Console.WriteLine("Норма кватерніона q1: {0}", magnitude);
+
+       
+        Quaternion conjugate = q1.Conjugate();
+        Console.WriteLine("Спряжений кватерніон q1: W={0}, X={1}, Y={2}, Z={3}", conjugate.W, conjugate.X, conjugate.Y, conjugate.Z);
+
+      
+        Quaternion inverse = q1.Inverse();
+        Console.WriteLine("Інверсний кватерніон q1: W={0}, X={1}, Y={2}, Z={3}", inverse.W, inverse.X, inverse.Y, inverse.Z);
+
+      
+        double[,] rotationMatrix = q1.ToRotationMatrix();
+        Console.WriteLine("Матриця обертання з кватерніона q1:");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"{rotationMatrix[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Quaternion.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6._3
+{
+    public class Quaternion
+    {
+        public double W { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Quaternion(double w, double x, double y, double z)
+        {
+            W = w;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // Перевантаження додавання
+        public static Quaternion operator +(Quaternion a, Quaternion b)
+        {
+            return new Quaternion(a.W + b.W, a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        // Перевантаження віднімання
+        public static Quaternion operator -(Quaternion a, Quaternion b)
+        {
+            return new Quaternion(a.W - b.W, a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        // Перевантаження множення
+        public static Quaternion operator *(Quaternion a, Quaternion b)
+        {
+            double w = a.W * b.W - a.X * b.X - a.Y * b.Y - a.Z * b.Z;
+            double x = a.W * b.X + a.X * b.W + a.Y * b.Z - a.Z * b.Y;
+            double y = a.W * b.Y - a.X * b.Z + a.Y * b.W + a.Z * b.X;
+            double z = a.W * b.Z + a.X * b.Y - a.Y * b.X + a.Z * b.W;
+            return new Quaternion(w, x, y, z);
+        }
+
+        // Перевантаження порівняння
+        public static bool operator ==(Quaternion a, Quaternion b)
+        {
+            return a.W == b.W && a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+
+        public static bool operator !=(Quaternion a, Quaternion b)
+        {
+            return !(a == b);
+        }
+
+        // Метод для обчислення кватерніона
+        public double Magnitude()
+        {
+            return Math.Sqrt(W * W + X * X + Y * Y + Z * Z);
+        }
+
+        // Метод для обчислення спряженого кватерніона
+        public Quaternion Conjugate()
+        {
+            return new Quaternion(W, -X, -Y, -Z);
+        }
+
+        // Метод для обчислення інверсного кватерніона
+        public Quaternion Inverse()
+        {
+            Quaternion conjugate = Conjugate();
+            double magnitude = Magnitude();
+            double magnitudeSquared = magnitude * magnitude;
+
+            return new Quaternion(conjugate.W / magnitudeSquared, conjugate.X / magnitudeSquared, conjugate.Y / magnitudeSquared, conjugate.Z / magnitudeSquared);
+        }
+
+        // Метод для конвертації кватерніона в матрицю обертання
+        public double[,] ToRotationMatrix()
+        {
+            double[,] matrix = new double[3, 3];
+
+            double xx = X * X;
+            double yy = Y * Y;
+            double zz = Z * Z;
+            double xy = X * Y;
+            double xz = X * Z;
+            double yz = Y * Z;
+            double wx = W * X;
+            double wy = W * Y;
+            double wz = W * Z;
+
+            matrix[0, 0] = 1 - 2 * (yy + zz);
+            matrix[0, 1] = 2 * (xy - wz);
+            matrix[0, 2] = 2 * (xz + wy);
+
+            matrix[1, 0] = 2 * (xy + wz);
+            matrix[1, 1] = 1 - 2 * (xx + zz);
+            matrix[1, 2] = 2 * (yz - wx);
+
+            matrix[2, 0] = 2 * (xz - wy);
+            matrix[2, 1] = 2 * (yz + wx);
+            matrix[2, 2] = 1 - 2 * (xx + yy);
+
+            return matrix;
+        }
+    }
+
+}
